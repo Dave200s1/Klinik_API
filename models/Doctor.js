952c837f0b9efc1d3fba1707640fbe7bc9861a07
David <d.stefanov16@icloud.com>
2026-02-21 16:19:36 +0100

    creating a Doctor model

---
+++ models/Doctor.js
@@ -0,0 +1,26 @@
+const {EntitySchema} = require("typeorm")
+
+module.export = new EntitySchema({
+    name: "Doctor",
+    tableName: "doctors",
+    columns:{
+        id: {
+            primary: true,
+            type: "integer",
+            generated: true,
+        },
+        firstname: {
+            type: "varchar",
+        },
+        lastname:{
+            type: "varchar",
+        },
+        address:{
+            type: "varchar",
+        },
+        email:{
+            type: "varchar",
+            unique: true,
+        },
+    },
+})
